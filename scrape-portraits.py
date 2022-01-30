c3a7ec8c1640828cc41beb819f3f7917b1d70deb
Jen Chan <6406037+usrrname@users.noreply.github.com>
2022-01-30 15:50:30 -0500

    creates folders with names scraped

--- scrape-portraits.py
+++ scrape-portraits.py
@@ -1,34 +1,27 @@
 import os
 import csv
-import requests
 
 parent_dir = 'data-set'
 file = 'ss-names.csv'
 path = os.path.abspath(file)
 image_links = []
 
-with open(file) as csv_file:
+with open(file, 'r') as csv_file:
     data = csv.reader(csv_file, delimiter=',')
-    line_count = 0
 
+    if os.path.isdir(parent_dir) == False:
+        os.mkdir(parent_dir)
+    os.chdir(parent_dir)
+
+    i = 0
     for row in data:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
+
+        name = row[i].replace(' ', '_')
+        print(name)
+
+        if os.path.isdir(name):
+            print(f'A folder called {name} already exists')
+
         else:
-            if os.path.isdir(parent_dir):
-                os.mkdir('data-set')
-                os.chdir('data-set')
-            else:
-                os.chdir('data-set')
-
-            folder_name = row[line_count-1].split(
-                ', ')[line_count-1].replace(' ', '_')
-
-            if os.path.isdir(folder_name) == False:
-                print(f'\nCreating folder: {folder_name}')
-                os.mkdir(folder_name)
-                os.chdir(folder_name)
-                line_count += 1
-            else:
-                print(f'{folder_name} already exists')
+            print(f'\nCreating folder: {name}')
+            os.mkdir(name)
