263c2d8ae8cbafbc7c264b681408d8eb323a8ec5
Jen Chan <6406037+usrrname@users.noreply.github.com>
2022-02-21 12:23:35 -0500

    fix: closes handler after saving image

--- image-scraper.py
+++ image-scraper.py
@@ -1,3 +1,4 @@
+import requests
 from serpapi import GoogleSearch
 from dotenv import load_dotenv
 import os
@@ -5,8 +6,12 @@ load_dotenv()
 
 token = os.environ.get('SERP_API_KEY')
 
+headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0",
+           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+           "Accept-Language": "en-US,en;q=0.9"}
 
-def scrape_image_urls(query_string, number_of_images):
+
+def scrape_images(query_string, number_of_images):
     image_urls = []
     params = {
         "api_key": token,
@@ -14,7 +19,7 @@ def scrape_image_urls(query_string, number_of_images):
         "ijn": "0",
         "start": 0,
         "num": number_of_images,
-        "q": query_string + 'portrait',
+        "q": query_string + ' portrait',
         "google_domain": "google.com",
         "tbm": "isch",
         'tbs': 'images'
@@ -23,7 +28,21 @@ def scrape_image_urls(query_string, number_of_images):
     results = search.get_dict()
     image_results = results['images_results']
 
-    for image in image_results:
-        image_urls += [image['original']]
+    path = os.path.abspath('data-set/' + query_string.replace(' ', '_'))
 
-    return image_urls
+    for image in image_results:
+        image_name = image['source'].strip()
+        image_urls.append(tuple((image_name, image['original'])))
+
+    for (img_name, img_url) in image_urls:
+        str_arr = str(img_url).split('.')
+        print(str_arr)
+        img_ext = '.' + str_arr[-1]
+        print(img_ext)
+        img_data = requests.get(img_url, headers=headers).content
+        with open(path + "/" + img_name + img_ext, 'wb') as handler:
+            handler.write(img_data)
+        handler.close()
+
+
+scrape_images('Adolf Hitler', 5)
