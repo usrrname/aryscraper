f68b2774b98a539303848120efb1d5753ab00ffd
Jen Chan <6406037+usrrname@users.noreply.github.com>
2022-03-19 14:30:23 -0400

    refactor: turn names in csv into list instead of looping over csv.reader obj

--- create-named-folders.py
+++ create-named-folders.py
@@ -6,25 +6,28 @@ parent_dir = 'data-set'
 file = 'ss-names.csv'
 path = os.path.abspath(file)
 
-# creates the folders for data-set if they don't already exist
 with open(file, 'r') as csv_file:
-    data = csv.reader(csv_file, delimiter=',')
 
+    data = csv.reader(csv_file, delimiter=',')
+    names = [x for x in data if x != 'Name']
+
+    # if data-set folder does not exist, create it
     if os.path.isdir(parent_dir) == False:
         os.mkdir(parent_dir)
     os.chdir(parent_dir)
 
-    i = 0
-    for row in data:
-        raw_name = row[i].strip()
-        name = row[i].replace(' ', '_')
+    index = 1
+    while index < len(names):
 
-        if os.path.isdir(name):
+        name = ''.join(names[index])
+        print(name)
+        # if folder already exists; scrape 100 images
+        if os.path.isdir(name) and name != 'Name':
             print(f'A folder called {name} already exists')
             os.chdir(name)
-            scrape_images(raw_name, 100)
+            scrape_images(name, 100)
             os.chdir('..')
+        # creates the folders for data-set if they don't already exist
         else:
-            if name != 'Name':
-                print(f'\nCreating folder: {name}')
-                os.mkdir(name)
+            print(f'\nCreating folder: {name}')
+            os.mkdir(name)
