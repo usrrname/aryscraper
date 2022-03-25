06c2a239c7004561e597fd67bc164c04edeade16
Jen Chan <6406037+usrrname@users.noreply.github.com>
2022-03-25 08:54:20 -0400

    adds util functions for returning image extensions and finding complete image links to download from

--- util.py
+++ util.py
@@ -9,7 +9,7 @@ def remove_contents_in_brackets(line):
 
 
 def hasExtension(file):
-    file = file.lower()
+    file = str(file.lower())
     image_extensions = ['.png', '.jpg', '.jpeg', '.tiff',
                         '.bmp', '.gif', '.webp', '.svg']
     img_ext = ('.png', '.jpg', '.jpeg', '.tiff',
@@ -18,3 +18,35 @@ def hasExtension(file):
         return True
     else:
         return False
+
+
+def get_image_link(image_url):
+    url = str(image_url)
+    extensions = ['.png', '.jpg', '.jpeg', '.tiff',
+                          '.bmp', '.gif', '.webp', '.svg']
+    contains_extension = [ext in url for ext in extensions]
+    for ext in extensions:
+        if url.endswith(ext):
+            image_format = url.split('.')[-1]
+            return image_format
+        elif any(ext in url for ext in extensions):
+            truthy_index = contains_extension.index(True)
+            position = url.find(extensions[truthy_index])
+            full_image_link = url[:position+len(extensions[truthy_index])]
+            return full_image_link
+
+
+def get_image_format(image_url):
+    url = str(image_url)
+    extensions = ['.png', '.jpg', '.jpeg', '.tiff',
+                          '.bmp', '.gif', '.webp', '.svg']
+    contains_extension = [ext in url for ext in extensions]
+    for ext in extensions:
+        if url.endswith(ext):
+            image_format = url.split('.')[-1]
+            return image_format
+        elif any(ext in url for ext in extensions):
+            truthy_index = contains_extension.index(True)
+            position = url.find(extensions[truthy_index])
+            image_format = url[position:position+len(extensions[truthy_index])]
+            return image_format
