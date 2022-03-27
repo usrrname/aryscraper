352c0f2b884c5d1490fe26551f2e9f29609c29df
Jen Chan <6406037+usrrname@users.noreply.github.com>
2022-03-27 17:04:29 -0400

    refactor: extract data set folder creation function into a separate function perf: only make Serpapi google image search call if a folder doesnt have enough images feat: complete ability to switch folders and make new image downloads sequentially

--- scraper.py
+++ scraper.py
@@ -1,10 +1,11 @@
-import imghdr
+from unittest import skip
 import requests
 from serpapi import GoogleSearch
 from dotenv import load_dotenv
+from pathvalidate import sanitize_filename
 import os
 
-from util import hasExtension
+from util import get_image_format, get_image_link, has_extension
 load_dotenv()
 
 token = os.environ.get('SERP_API_KEY')
@@ -30,26 +31,59 @@ def scrape_images(query_string, number_of_images):
     search = GoogleSearch(params)
     results = search.get_dict()
     image_results = results['images_results']
-    path = os.getcwd()
 
-    for image in image_results:
+    while len(image_urls) < number_of_images:
+        for image in image_results:
+            if (image['original'] and has_extension(image['original'])):
+                image_name = sanitize_filename(
+                    image['title']).replace(' ', '_')
+                image_urls.append(tuple((image_name, image['original'])))
+            else:
+                position = image['position']
+                print(
+                    f'{position}th search result does not have a valid image extension or its original image url does not exist')
+                break
+    return image_urls
 
-        if (image['original'] and hasExtension(image['original'])):
-            image_name = image['title'].strip().replace(
-                ' ', '_').replace('-', '_').replace(',', '').replace('.', '').replace('/', '').replace('|', '').replace('/', '')
-            image_urls.append(tuple((image_name, image['original'])))
-            continue
-        else:
-            position = image['position']
-            print(
-                f'image at position {position} does not have a valid image extension or original url does not exist')
 
-    for image_name, image_url in image_urls:
-        print(f'\nDownloading image: {image_url}')
-        img_ext = '.' + str(image_url).split('.')[-1] or '.jpg'
-        img_data = requests.get(image_url, headers=headers).content
-        filename = image_name + img_ext
-        with open(path + "/" + image_name + img_ext, 'wb') as handler:
-            handler.write(img_data)
-            print(f'\n Successfully saved: {filename}')
-            handler.close()
+def download_images(query, number_of_images):
+    current_path = os.getcwd()
+    number_of_files_in_folder = len(next(os.walk(current_path))[2])
+
+    print(
+        f'currently {number_of_files_in_folder} / {number_of_images} files in {os.getcwd()}')
+
+    if number_of_files_in_folder >= number_of_images:
+        print(
+            f'{number_of_files_in_folder} files in folder, only require {number_of_images} images\nFolder contains enough images. Skipping download.')
+        return
+
+    elif number_of_files_in_folder < number_of_images:
+        print(query)
+        image_urls = scrape_images(query, number_of_images)
+
+        for image_name, image_url in image_urls:
+
+            print(f'\nDownloading: {image_url}')
+
+            # get the image extension
+            img_ext = get_image_format(image_url)
+            img_data = requests.get(get_image_link(
+                image_url), headers=headers).content
+            filename = image_name + img_ext
+
+            if os.path.isdir(filename):
+                print(f'{filename} already exists')
+                break
+            else:
+                with open(current_path + "/" + image_name + img_ext, 'wb') as handler:
+                    try:
+                        handler.write(img_data)
+                        print(f'\n Successfuly Saved: {filename}')
+                    except KeyError as e:
+                        print(
+                            f'\nMapping of original key not found: {KeyError}')
+                    except Exception as e:
+                        print(f'\n Failed to Save: {filename}')
+
+                    handler.close()
