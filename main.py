b11b29a0ca27dc817ef8b9b18c2ea5187391183d
Jen Chan <6406037+usrrname@users.noreply.github.com>
2022-04-23 10:17:34 -0400

    refactor: always update list of SS names before scraping images

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+from scrape_names import scrape_ss_names
 from folder_structure import create_folders, create_data_set
-from names_from_csv import get_names_from_csv
+from util import get_names_from_csv
 
 names = get_names_from_csv('ss-names.csv')
 
 
 def process():
-
+    filename = 'ss-names.csv'
+    url = 'https://en.wikipedia.org/wiki/List_of_SS_personnel'
+    header = ['Name']
+    print("Scraping SS names...")
+    scrape_ss_names(url, filename, header)
     print("Creating folders...")
     create_folders('data-set', names)
+    print("Done!")
 
 
 def main():
