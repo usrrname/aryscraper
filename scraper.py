01c08dfaa31575b276a0dbdb2a0b05840cde82b1
Jen <6406037+usrrname@users.noreply.github.com>
2022-06-08 10:10:59 -0400

    refactor(scraper.py): scraped image folders that dont have the max number of imgs due to previous fails should pick up off the previous index

--- scraper.py
+++ scraper.py
@@ -18,20 +18,21 @@ headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/2010010
            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
            "Accept-Language": "en-US,en;q=0.5"}
 
-ambiguous_names = ['Adam Grünewald', 'Anton Burger',
+ambiguous_names = ['Adam Grünewald', 'Anton Burger', 'Carl_Værnet'
                    'Karl Otto von Salisch', 'Yuri Frolov']
+
+name_only = ['Adolf Ax', 'Adolf Maurer']
 
 
 def scrape_images(name, number_of_images, start_number):
     name = str(name)
     query = ''
 
-    if any(name for word in ambiguous_names):
+    if name in ambiguous_names:
         # clarify search for names that are common
         query = name + ' nazi'
-
-    else:
-        query = name + (' portrait')  # standard query string
+    elif name in name_only:
+        query = name
 
     params = {
         "api_key": token,
@@ -53,6 +54,10 @@ def scrape_images(name, number_of_images, start_number):
         urllib.request.urlretrieve(
             results['search_metadata']['json_endpoint'], 'data.json')
         print(f'data.json for {query} saved!')
+    except FileNotFoundError in Exception as e:
+        print(f'{FileNotFoundError()}: {e}')
+        pass
+    try:
         filtered_images = [
             image for image in image_results if image['original']]
     except KeyError in Exception as e:
@@ -62,66 +67,67 @@ def scrape_images(name, number_of_images, start_number):
     return filtered_images
 
 
-def save_images(name, image_urls, next_image_index=0):
-    if next_image_index != 0:
-        i = next_image_index
+def save_images(name, image_urls, last_image_index):
 
     for i, image in enumerate(image_urls):
+        if i >= last_image_index:
+            # get the image extension
+            img_ext = get_image_format(image['original'])
 
-        # get the image extension
-        img_ext = get_image_format(image['original'])
+            if img_ext not in image_extensions or None:
+                continue
 
-        if img_ext not in image_extensions or None:
-            continue
+            title = image['title']
+            img_position = image['position']  # position in search results
+            file_title = sanitize_filename(name).lower().replace(
+                ' ', '').strip('_')[:6]  # trim the file title for consistency
 
-        title = image['title']
-        img_position = image['position']  # position in search results
-        file_title = sanitize_filename(name).lower().replace(
-            ' ', '').strip('_')[:6]  # trim the file title for consistency
+            filename = f'{i+1}-{file_title}{img_ext}'
+            data_row = [name, filename, title,
+                        img_position, image['original']]
+            try:
+                current_path = os.getcwd()
+                with open(current_path + "/" + filename, 'wb') as handler:
+                    print(f'\nDownloading: {image["original"]}')
+                    img_data = requests.get(get_image_link(
+                        image['original']), headers=headers).content
+                    handler.write(img_data)
+                    print(f'\n Successfuly Saved: {filename}')
+                    handler.close()
+            except KeyError as e:
+                print(
+                    f'\nMapping of original key not found: {KeyError}, {e}')
+            except Exception as e:
+                print(f'{e}')
+                continue
 
-        filename = f'{i}-{file_title}{img_ext}'
-        data_row = [name, filename, title,
-                    img_position, image['original']]
-        try:
-            current_path = os.getcwd()
-            with open(current_path + "/" + filename, 'wb') as handler:
-                print(f'\nDownloading: {image["original"]}')
-                img_data = requests.get(get_image_link(
-                    image['original']), headers=headers).content
-                handler.write(img_data)
-                print(f'\n Successfuly Saved: {filename}')
-                handler.close()
-        except KeyError as e:
-            print(
-                f'\nMapping of original key not found: {KeyError}, {e}')
-        except Exception as e:
-            print(f'{e}')
-            continue
-
-        # add info about saved image
-        try:
-            if is_already_saved(data_row, 'data-info.csv') == False:
-                add_row_to_csv(current_path + '/' + 'data-info.csv', data_row)
-        except Exception as e:
-            print(f'\n Failed to Save: data-info.csv at the {i}th file, {e}')
+            # add info about saved image
+            try:
+                if is_already_saved(data_row, 'data-info.csv') == False:
+                    add_row_to_csv(current_path + '/' +
+                                   'data-info.csv', data_row)
+            except Exception as e:
+                print(
+                    f'\n Failed to Save: data-info.csv at the {i}th file, {e}')
+                continue
 
 
 def lookup_recent_row(csv_file):
     # finds image position in returned response
     most_recent_row = last_row_from_csv(csv_file)
-    last_image_index = int(most_recent_row[3])
 
-    # gets last index used to prepend filename
-    next_image_index = int(most_recent_row[1].split('-')[0]) + 1
-    print(
-        f'last image index: {last_image_index}\n next image index: {next_image_index}')
-    return next_image_index, last_image_index
+    # if it hasnt been parsed yet start at the beginning
+    if most_recent_row == ['Name', 'Filename', 'Title', 'Position', 'Original URL']:
+        last_image_index = 1
+
+    else:  # if there are already saved rows, gets last index used to prepend filename
+        last_image_index = int(most_recent_row[3])
+
+    return last_image_index
 
 
-def download_images(name, number_of_images):
-
-    write_csv_header(
-        'data-info.csv', ["Name", "Filename", "Title", "Position", "Original URL"])
+def list_images_in_folder(number_of_images, folder_name):
+    os.chdir(folder_name)
 
     files = os.listdir(os.getcwd())
 
@@ -138,20 +144,24 @@ def download_images(name, number_of_images):
         return
 
     else:
-        remainder = 100 - number_of_files_in_folder
+        remainder = number_of_images - number_of_files_in_folder
         print(
             f'{number_of_files_in_folder} files in folder, need {remainder} more images')
 
-        start_number = 0
-        image_urls = []
+    return number_of_files_in_folder
 
-        if os.path.isfile('data.json'):
-            last_image_index, next_image_index = 1, 1
-            image_results = read_json('data.json')
-            image_urls = [
-                image for image in image_results if image['original'] and image['position'] > last_image_index]
-            save_images(name, image_urls, next_image_index)
-        else:
-            start_number = 1
-            image_urls = scrape_images(name, number_of_images, start_number)
-            save_images(name, image_urls)
+
+def download_images(name, number_of_images):
+
+    if os.path.isfile('data.json') and os.path.isfile('data-info.csv'):
+        last_image_index = lookup_recent_row('data-info.csv')
+        image_results = read_json('data.json')
+        image_urls = [
+            image for image in image_results if image['position'] > last_image_index]
+        save_images(name, image_urls, last_image_index)
+    else:
+        write_csv_header(
+            'data-info.csv', ["Name", "Filename", "Title", "Position", "Original URL"])
+
+        image_urls = scrape_images(name, number_of_images, 1)
+        save_images(name, image_urls, 0)
